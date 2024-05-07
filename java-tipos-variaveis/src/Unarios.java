66be7eaf81e4665fe374afc19424ee9db6ddfd71
RenanCarlosRocha <rc882041@gmail.com>
2024-05-07 12:17:07 -0300

    adicionado exemplos de incrementação e inversão de valores booleanos

--- java-tipos-variaveis/src/Unarios.java
+++ java-tipos-variaveis/src/Unarios.java
@@ -7,5 +7,17 @@ public class Unarios {
 
         numero = numero * -1;
         System.out.println(numero);
+
+        numero = numero++; // a ordem está errada
+        numero = ++numero;
+        System.out.println(numero);
+
+        boolean variavel = true;
+
+        System.out.println(!variavel);
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
     }
 }
